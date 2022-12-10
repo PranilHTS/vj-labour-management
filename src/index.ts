fc0b7d2be1f4796e3bf49f7b7ce5682eab6d0208
Pranil <pranil.ingle@htstechsolutions.com>
2022-12-10 09:49:42 +0530

    access control

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,24 @@ console.log('process.env.pm_id', process.env.pm_id);
 
         // Call midlewares
         app.use(helmet());
+        app.use(function (req, res, next) {
 
+            // Website you wish to allow to connect
+            res.setHeader('Access-Control-Allow-Origin', '*');
+        
+            // Request methods you wish to allow
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        
+            // Request headers you wish to allow
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            res.setHeader('Access-Control-Allow-Credentials', 'true');
+        
+            // Pass to next layer of middleware
+            next();
+        });
         app.use(express.json());
         // load global pre middlewares here
         app.set('base', contextPath);
